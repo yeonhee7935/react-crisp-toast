3e86726358380f058a9cc45a3bb4052fc0e454cd
Yuni <60534152+yeonhee7935@users.noreply.github.com>
2024-12-21 05:05:49 +0000

    :recycle: implement example2

--- examples/example2/src/App.tsx
+++ examples/example2/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useToast} from "react-crisp-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {addToast} = useToast()
+  const showToast = ()=>{
+    addToast({
+      message: "Hello world" 
+    })
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div style={{height:"90vh",flexDirection:"column", display:"flex", alignItems:"center", justifyContent:"center"}}>
+     
+      <img src="../favicon.png" alt="logo" width={300}/>
+      <br/>
+      <button
+        onClick={showToast}
+        style={{
+          padding: "15px 30px",
+          fontSize: "18px",
+          backgroundColor: "#FFD700",
+          color: "#121212",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          fontWeight: "bold",
+          transition: "background-color 0.3s",
+        }}
+      >Show Toast</button> 
+  </div>)
 }
 
 export default App
